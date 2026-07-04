b6ee654c65a84a19b14b86bf6192189717161953
Boxfort <jackwanderson@protonmail.com>
2026-07-04 22:25:49 +0100

    continue procgen

--- src/map/generator.rs
+++ src/map/generator.rs
@@ -8,18 +8,43 @@ use rand::{Rng, RngExt, SeedableRng};
 
 const OVERMAP_CHUNK_SIZE: i32 = 2048;
 
+struct GameMap {
+    overmap_chunks: HashMap<OvermapChunkCoords, OvermapChunk>,
+}
+
+#[derive(Eq, Hash, PartialEq, Clone, Copy)]
 struct OvermapChunkCoords(IVec2);
+
 struct OvermapChunk {
     coordinates: OvermapChunkCoords,
     cities: HashSet<City>,
     roads: RoadGraph,
-    structure: HashSet<City>,
+}
+
+impl OvermapChunk {
+    pub fn new(coordinates: OvermapChunkCoords) -> OvermapChunk {
+        OvermapChunk {
+            coordinates,
+            cities: HashSet::new(),
+            roads: RoadGraph::new(),
+        }
+    }
 }
 
 struct RoadGraph {
     nodes: Vec<RoadNode>,
     edges: Vec<RoadEdge>,
     connections: HashMap<RoadNodeId, Vec<(OvermapChunkCoords, RoadEdgeId)>>,
+}
+
+impl RoadGraph {
+    pub fn new() -> RoadGraph {
+        RoadGraph {
+            nodes: vec![],
+            edges: vec![],
+            connections: HashMap::new(),
+        }
+    }
 }
 
 struct RoadEdgeId(i32);
@@ -35,13 +60,21 @@ struct RoadNode {
     position: IVec2,
 }
 
+#[derive(Eq, Hash, PartialEq)]
 struct CityId(i32);
 
+#[derive(Eq, Hash, PartialEq)]
 struct City {
     id: CityId,
     position: IVec2,
     size: i32, // The estimated size of the city, used for influencing the procgen
-    connected_to: HashSet<(OvermapChunkCoords, CityId)>,
+    connected_to: Vec<CityConnection>,
+}
+
+#[derive(PartialEq, Eq, Hash)]
+struct CityConnection {
+    overmap_coordinate: OvermapChunkCoords,
+    city_id: CityId
 }
 
 struct Building {
@@ -50,36 +83,72 @@ struct Building {
 }
 
 pub fn generate_map(current_overmap_coordinate: IVec2) {
+    let mut game_map = GameMap {
+        overmap_chunks: HashMap::new(),
+    };
+
     let neighbouring_chunk_positions =
         get_neighbouring_overmap_chunk_positions(current_overmap_coordinate);
     let mut rng = ChaCha8Rng::seed_from_u64(42); // TODO: remove and use the resource
 
     let cities: Vec<City> = vec![];
 
-    for chunk_pos in neighbouring_chunk_positions {
+    for chunk_coordinate in neighbouring_chunk_positions {
+        let mut overmap_chunk = OvermapChunk::new(chunk_coordinate);
+
         if rng.random_range(0..=1) == 1 {
             // Add a city somewhere
             let pos = IVec2 {
-                x: rng.random_range(0..2048),
-                y: rng.random_range(0..2048),
+                x: rng.random_range(0..OVERMAP_CHUNK_SIZE),
+                y: rng.random_range(0..OVERMAP_CHUNK_SIZE),
             };
 
             let city = City {
                 id: CityId(cities.len() as i32),
                 position: pos,
                 size: 512,
-                connected_to: HashSet::new(),
+                connected_to: vec![],
             };
+
+            overmap_chunk.cities.insert(city);
+
+            // Add connecting road to adjacent cities.
+            let adjacent_chunks = get_potential_neighbouring_city_overmap_chunks(chunk_coordinate.0);
+
+            for city_chunk in adjacent_chunks {
+                // Does it contain a city
+                let chunk = game_map.overmap_chunks.get(&city_chunk).unwrap();
+
+                if !chunk.cities.is_empty() {
+                    // Add connecting road
+                }
+            }
         }
+
+        game_map.overmap_chunks.insert(
+             chunk_coordinate,
+            overmap_chunk
+        );
     }
 }
 
-fn get_neighbouring_overmap_chunk_positions(chunk_coordinate: IVec2) -> Vec<IVec2> {
-    let mut adjacent_positions: Vec<IVec2> = vec![];
+fn get_potential_neighbouring_city_overmap_chunks(chunk_coordinate: IVec2) -> Vec<OvermapChunkCoords> {
+    let mut adjacent_positions: Vec<OvermapChunkCoords> = vec![];
+
+    adjacent_positions.push(OvermapChunkCoords(chunk_coordinate + IVec2 { x: 1, y: 0 }));
+    adjacent_positions.push(OvermapChunkCoords(chunk_coordinate + IVec2 { x: -1, y: 0 }));
+    adjacent_positions.push(OvermapChunkCoords(chunk_coordinate + IVec2 { x: 0, y: 1 }));
+    adjacent_positions.push(OvermapChunkCoords(chunk_coordinate + IVec2 { x: 0, y: -1 }));
+
+    adjacent_positions
+}
+
+fn get_neighbouring_overmap_chunk_positions(chunk_coordinate: IVec2) -> Vec<OvermapChunkCoords> {
+    let mut adjacent_positions: Vec<OvermapChunkCoords> = vec![];
 
     for x in -2..=2 {
         for y in -2..=2 {
-            adjacent_positions.push(chunk_coordinate + IVec2 { x, y });
+            adjacent_positions.push(OvermapChunkCoords(chunk_coordinate + IVec2 { x, y }));
         }
     }
 
